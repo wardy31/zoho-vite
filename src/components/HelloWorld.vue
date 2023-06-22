0d78cd08bb35bd9f0c13a70bb299092b304cf992
wardy31 <warded858@gmail.com>
2023-06-22 15:11:39 +0800

    deploy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,18 +19,7 @@ const count = ref(0)
     </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h1>Zoho Deployment</h1>
 </template>
 
 <style scoped>
